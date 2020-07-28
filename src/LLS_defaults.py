aac4e84990b5750466a5f94483a246e9563baa64
OscarCunningham <35840763+OscarCunningham@users.noreply.github.com>
2020-07-28 18:25:45 +0100

    Update LLS_defaults.py
    Add kissat to solvers

--- src/LLS_defaults.py
+++ src/LLS_defaults.py
@@ -2,5 +2,5 @@ verbosity = 2 # 0, 1, 2 or 3
 pattern_output_format = "rle" # "rle" or "csv"
 life_encoding_method = 1 # 0, 1 or 2
 rulestring = "B3/S23" # Any valid rulestring
-solver = "glucose-syrup" # "minisat", "MapleCOMSPS", "MapleCOMSPS_LRB", "riss", "glucose", "glucose-syrup", "lingeling", "plingeling", "treengeling"
+solver = "kissat" # "kissat", "cadical", "minisat", "MapleCOMSPS", "MapleCOMSPS_LRB", "riss", "glucose", "glucose-syrup", "lingeling", "plingeling", "treengeling"
 background = "possible_strobing" # Any file in backgrounds/
